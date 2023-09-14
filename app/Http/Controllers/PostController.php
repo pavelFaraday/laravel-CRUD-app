ae3afd2335d6d732ae2eb244f17085ec033f3548
Giorgi Epitashvili <devstudio.corp@gmail.com>
2023-09-14 14:12:28 +0400

    79. Creating Form (Part - 1)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -16,18 +16,32 @@ class PostController extends Controller
 
     /**
      * Show the form for creating a new resource.
+     * 
+     * 
      */
     public function create()
     {
-        return view('create');
+        $categories = Category::all();
+        return view('create', compact('categories'));
     }
 
     /**
      * Store a newly created resource in storage.
+     * 
+     *
      */
+
     public function store(Request $request)
     {
-        //
+        // return "Hello";
+        $request->validate([
+            'image'       => ['required', 'max:2028', 'image'],
+            'title'       => ['required', 'max: 255'],
+            'category_id' => ['required', 'integer'],
+            'description' => ['required']
+        ]);
+
+        dd('success');
     }
 
     /**
@@ -43,7 +57,7 @@ class PostController extends Controller
      */
     public function edit(string $id)
     {
-        return view('edit');
+        //
     }
 
     /**
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
     <div class="main-content mt-5">
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <div class="alert alert-danger">
+                    {{ $error }}
+                </div>
+            @endforeach
+        @endif 
         <div class="card">
             <div class="card-header">
                 <div class="row">
@@ -14,30 +21,34 @@
                 </div>
               </div>
 
+
+
             <div class="card-body">
-                <form action="">
+                <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
                     <div class="form-group">
                         <label for="" class="form-label">Image</label>
-                        <input type="file" class="form-control">
+                        <input type="file" name="image" class="form-control">
                     </div>
                     <div class="form-group">
                         <label for="" class="form-label">Title</label>
-                        <input type="text" class="form-control">
+                        <input type="text" name="title" class="form-control">
                     </div>
                     <div class="form-group">
-                        <label for="" class="form-label">Option</label>
-                        <select name="" id="" class="form-control">
-                            <option value="">test1</option>
-                            <option value="">test2</option>
-                            <option value="">test3</option>
+                        <label for="" class="form-label">Category</label>
+                        <select name="category_id" id="" class="form-control">
+                            <option value="">Select</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            @endforeach
                         </select>
                     </div>
                     <div class="form-group">
                         <label for="" class="form-label">Description</label>
-                        <textarea name=""  class="form-control"></textarea>
+                        <textarea name="description"  class="form-control"></textarea>
                     </div>
                     <div class="form-group mt-3">
-                        <button class="btn btn-primary">Submit</button>
+                        <button type="submit" class="btn btn-primary">Submit</button>
                     </div>
                 </form>
             </div>
